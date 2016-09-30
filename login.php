cf8b8d9a86760802bc330677dec6afc9bc4f715a
paladhidinesh <dinesh.paladhi@gmail.com>
2016-09-30 12:39:59 -0400

    Revert "Revert "login first trail""
    This reverts commit ad46e01e0214ffc483563bccde03ca789677a1a3.

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ANSWERSKART- Login Page</title>
+	<link rel="stylesheet" type="text/css" href="loginstyle.css">
+</head>
+<body>
+	<header>
+		<h1 id ="heading">ANSWERSKART</h1>
+	</header>
+	<div id="loginform">
+		<p><b>LOGIN: </b></p>
+			<form action="main_home.php" method="post">
+				<table border="1" cellpadding="5" cellspacing="1">
+					<tr>
+						<td style="width: 100px;">Username:	</td>
+						<td style="width: 150px;"><input type="text" name="username"></td>
+					</tr>
+					<tr>
+						<td style="width: 100px;">Password:	</td>
+						<td style="width: 150px;"><input type="password" name="password"></td>
+					</tr>
+					<tr> 
+						<td></td>
+						<td><input type="submit" id="button" value="Login"></td>
+					</tr>
+				</table>
+			</form>
+	</div>
+				</table>
+	
+
+<footer>
+Construction Going on
+</footer>
+
+	</BODY>
+</html>
