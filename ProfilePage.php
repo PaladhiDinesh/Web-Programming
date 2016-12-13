05860a86f77bd2ed66af0cb79b4964cebf3488f6
paladhidinesh <dinesh.paladhi@gmail.com>
2016-12-13 12:00:09 +0530

    mile4

--- ProfilePage.php
+++ ProfilePage.php
@@ -23,11 +23,11 @@ Welcome to Answers Kart</h1><hr/>
 	$row1 = mysqli_fetch_array($nameresult,MYSQLI_ASSOC);
 
 	$dummy = $row1['user_id'];
-	$startArticle = ($_GET['page'] - 1) * 3;
-	$query = "SELECT questions_table.user_id as quser,title,tags,admin,question_id,questions_table.created_at,question_id FROM questions_table JOIN login_details ON login_details.user_id=questions_table.user_id where questions_table.user_id='".$dummy."'limit ".$startArticle.','.'5';
-					$result = mysqli_query($connection,$query) or die(" to query database".mysql_error());
+
+	$query = "SELECT questions_table.user_id as quser,title,tags,admin,question_id,questions_table.created_at,question_id FROM questions_table JOIN login_details ON login_details.user_id=questions_table.user_id where questions_table.user_id=".$dummy;
+					$result = mysqli_query($connection,$query) or die(" falied to query database".mysql_error());
 					$count=$result->num_rows;
-					$totalpages=ceil($count/3);	
+					$totalpages=ceil($count/5);	
 				// echo $totalpages;
 					if ($totalpages==0){
 						$totalpages=1;
@@ -46,8 +46,9 @@ Welcome to Answers Kart</h1><hr/>
 						$_GET['page']=$totalpages;
 					}
 
-				
-	
+					$startArticle = ($_GET['page'] - 1) * 5;
+	$paginationquery = "SELECT questions_table.user_id as quser,title,tags,admin,question_id,questions_table.created_at,question_id FROM questions_table JOIN login_details ON login_details.user_id=questions_table.user_id where questions_table.user_id='".$dummy."'limit ".$startArticle.','.'5';
+		$page_result = mysqli_query($connection,$paginationquery) or die ("Failed to query database".mysql_error());
 		?>
 
 <div class="container">
@@ -156,7 +157,7 @@ if ($uploadOk == 0) {
 
 	
 
-					while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+					while ($row = mysqli_fetch_array($page_result,MYSQLI_ASSOC)) {
 						$question_id=htmlentities($row['question_id']);
 						?>
 						<div class="row">
@@ -165,6 +166,7 @@ if ($uploadOk == 0) {
 								<h3><a href='single_question.php?ques_id=<?php echo $question_id; ?>'><?php echo htmlentities($row['title']);?></a></h3>
 								<h4>Tags:
 				 <?php
+				 	$newpagevalue=$row['admin'];
 					$onetag=explode(" ",$row['tags']);
 					foreach ($onetag as $value) {?>
 						<a href="tagspage.php?name=<?php echo $value;?>">
@@ -186,6 +188,7 @@ if ($uploadOk == 0) {
 								<b><?php echo "Asked by ";?><a href="ProfilePage.php?name=<?php echo trim($row['admin']);?>&page=1"> <?php echo htmlentities($row['admin']) ?></a>
 									<?php 
 										echo '(',scores($row['quser']),')'," on ".htmlentities($row['created_at'])."<br />"?> 
+
 								</b>
 							</p>
 						</div>
@@ -200,14 +203,16 @@ if ($uploadOk == 0) {
 						?>
 							<center><ul class="pagination">
 					<?php
+						// echo $row['admin'];
 					foreach (range(1,$totalpages) as $page){
-						// echo $page;
-						// echo "hi";
+							// echo $page;
+							// echo "hi";
 						if($page == $_GET['page']){
         					echo '<li><a class="active"><span class="currentpage">' . $page . '</span></a></li>';
         					// echo  "current";
     					}else if($page ==1 || $page ==$totalpages ||($page >= $_GET['page'] -2 && $page <= $_GET['page']+2)){
-							 echo '<li><a href="?page=' . $page . '">' . $page . '</a></li>'; 
+
+							 echo '<li><a href="?name='. $newpagevalue . '&page=' . $page . '">' . $page . '</a></li>'; 
 						}
 					}
 					?>
